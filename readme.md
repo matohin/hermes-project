557df2d182eb8b6154afdeab51cbd9ab76776b8a
Evgeniy Matohin <matohin@gmail.com>
2021-03-19 11:42:52 +0300

    kevault with RBAC

--- readme.md
+++ readme.md
@@ -9,3 +9,8 @@ pipenv lock -r > .\requirements.txt
 gc .\.gitignore > .\.funcignore
 
 New-AzResourceGroupDeployment -ResourceGroupName rg-hermes-project-dev-00 -TemplateParameterFile .\arm_templates\azuredeploy.parameters.dev-00.json -TemplateFile .\arm_templates\azuredeploy.json -Mode Complete -Force
+
+Get-AzSubscription
+Get-AzResourceGroup
+Get-AzRoleDefinition | Where-Object{$_.name -like "*search_for_roles*"}
+New-AzRoleAssignment -Scope /subscriptions/<subscription/rg/resource id> -RoleDefinitionId <role id>  -ObjectId <user/app id>
--- tests/int/test_key_vaut_access.py
+++ tests/int/test_key_vaut_access.py
@@ -2,32 +2,32 @@ import os
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
+if not os.getenv("KEY_VAULT_URI"):
+    os.environ["KEY_VAULT_URI"] = "https://kv-hermes-proj.vault.azure.net/"
+
+
 def test_keyvault_access():
 
-
-    keyVaultName = os.environ["KEY_VAULT_NAME"]
-    KVUri = f"https://{keyVaultName}.vault.azure.net"
+    key_vault_uri = os.environ["KEY_VAULT_URI"]
 
     credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=KVUri, credential=credential)
+    print(credential)
 
-    secretName = input("Input a name for your secret > ")
-    secretValue = input("Input a value for your secret > ")
+    client = SecretClient(vault_url=key_vault_uri, credential=credential)
+    print(client)
 
-    print(f"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...")
+    client.set_secret("secretName", "secretValue")
 
-    client.set_secret(secretName, secretValue)
+    # print(" done.")
 
-    print(" done.")
+    # print(f"Retrieving your secret from {keyVaultName}.")
 
-    print(f"Retrieving your secret from {keyVaultName}.")
+    # retrieved_secret = client.get_secret(secretName)
 
-    retrieved_secret = client.get_secret(secretName)
+    # print(f"Your secret is '{retrieved_secret.value}'.")
+    # print(f"Deleting your secret from {keyVaultName} ...")
 
-    print(f"Your secret is '{retrieved_secret.value}'.")
-    print(f"Deleting your secret from {keyVaultName} ...")
+    # poller = client.begin_delete_secret(secretName)
+    # deleted_secret = poller.result()
 
-    poller = client.begin_delete_secret(secretName)
-    deleted_secret = poller.result()
-
-    print(" done.")
+    # print(" done.")
