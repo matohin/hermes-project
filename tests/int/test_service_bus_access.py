b24a3a8d3061cd650f387b4b1f505dbcc40c27f5
Evgeniy Matohin <matohin@gmail.com>
2021-03-28 23:46:56 +0300

    service bus int test

--- tests/int/test_service_bus_access.py
+++ tests/int/test_service_bus_access.py
@@ -1,27 +1,29 @@
 import os
+import string
+from secrets import choice
 from azure.servicebus import ServiceBusClient, ServiceBusMessage
 
 
 def test_service_bus_access():
 
     connection_string = os.getenv("AzureWebJobsAzureSBConnection")
-    queue_name = "sbq-telegram-otput"
+    queue_name = "sbq-integration-test"
+
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    expected = "".join(choice(random_source) for i in range(20))
 
     with ServiceBusClient.from_connection_string(connection_string) as client:
 
-        with client.get_queue_sender(queue_name):
+        with client.get_queue_sender(queue_name) as sender:
 
-            expected = "Single message"
+            single_message = ServiceBusMessage(expected)
+            sender.send_messages(single_message)
 
-            message = ServiceBusMessage(expected)
-            queue_sender.send(message)
+        with client.get_queue_receiver(queue_name, max_wait_time=30) as receiver:
 
-        with client.get_queue_receiver(queue_name) as receiver:
+            msg = receiver.next()
+            actual = str(msg)
 
-            for msg in receiver:
+            assert actual == expected
 
-                actual = str(msg)
-
-                assert actual == expected
-
-                msg.complete()
+            receiver.complete_message(msg)
